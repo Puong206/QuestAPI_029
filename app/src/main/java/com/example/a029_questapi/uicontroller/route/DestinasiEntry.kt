4831e078172e7089c205932f1146a24903cfe6b6
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-20 03:47:27 +0700

    Membuat destinasi entry

---
+++ app/src/main/java/com/example/a029_questapi/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.a029_questapi.uicontroller.route
+
+import com.example.a029_questapi.R
+
+object DestinasiEntry {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
