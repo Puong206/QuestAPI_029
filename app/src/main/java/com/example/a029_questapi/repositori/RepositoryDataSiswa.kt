bf245852cc642c95e5fc174d486ebfd938be3860
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-20 01:34:11 +0700

    Membuat interface RepositoryDataSiswa

--- app/src/main/java/com/example/a029_questapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/a029_questapi/repositori/RepositoryDataSiswa.kt
@@ -1,2 +1,7 @@
 package com.example.a029_questapi.repositori
 
+import com.example.a029_questapi.modeldata.DataSiswa
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa() : List<DataSiswa>
+}
