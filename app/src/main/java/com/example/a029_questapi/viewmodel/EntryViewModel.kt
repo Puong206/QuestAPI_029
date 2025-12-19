62713f97a6f5ed7bcf9a830ca261860c0b1e04e5
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-20 03:26:03 +0700

    Menambah EntryViewModel

--- app/src/main/java/com/example/a029_questapi/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/a029_questapi/viewmodel/EntryViewModel.kt
@@ -1,2 +1,38 @@
 package com.example.a029_questapi.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.a029_questapi.modeldata.DetailSiswa
+import com.example.a029_questapi.modeldata.UIStateSiswa
+import com.example.a029_questapi.modeldata.toDataSiswa
+import com.example.a029_questapi.repositori.RepositoryDataSiswa
+import okhttp3.Response
+
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            val sip: Response<Void> = repositoryDataSiswa.postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+            if (sip.isSuccessful) {
+                println("Sukses Tambah Data : ${sip.message()}")
+            }else{
+                println("Gagal Tambah Data : ${sip.errorBody()}")
+            }
+        }
+    }
+}
