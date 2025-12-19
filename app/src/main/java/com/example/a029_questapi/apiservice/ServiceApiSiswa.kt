62d75005fcdeb96050a4b82eabcb1df578a09e7a
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-20 01:30:15 +0700

    Menambah fungsi untuk mengambil data siswa dari API

--- app/src/main/java/com/example/a029_questapi/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/a029_questapi/apiservice/ServiceApiSiswa.kt
@@ -4,5 +4,8 @@ import com.example.a029_questapi.modeldata.DataSiswa
 import retrofit2.http.GET
 
 interface ServiceApiSiswa {
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+    
 
 }
