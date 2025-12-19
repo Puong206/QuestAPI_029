c81146398bfcf38ff4020a0d657575a5234611a1
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-20 01:34:54 +0700

    Menambahkan fungsi postDataSiswa

--- app/src/main/java/com/example/a029_questapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/a029_questapi/repositori/RepositoryDataSiswa.kt
@@ -4,4 +4,5 @@ import com.example.a029_questapi.modeldata.DataSiswa
 
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa() : List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
 }
