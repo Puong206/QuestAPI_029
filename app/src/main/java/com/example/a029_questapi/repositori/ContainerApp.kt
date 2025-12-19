9c9db1f1fda1e9444bb70110347a7815c8651e3c
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-20 01:51:41 +0700

    Menambahkan HttpLoggingInterceptor

--- app/src/main/java/com/example/a029_questapi/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/a029_questapi/repositori/ContainerApp.kt
@@ -1,6 +1,15 @@
 package com.example.a029_questapi.repositori
 
+import okhttp3.logging.HttpLoggingInterceptor
+
 interface ContainerApp {
     val repositoryDataSiswa : RepositoryDataSiswa
 }
 
+class DefaultContainerApp : ContainerApp {
+    private val baseurl = ""
+
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+}
