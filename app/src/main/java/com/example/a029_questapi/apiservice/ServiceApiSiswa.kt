2260b5cddb62b238d4e6af01cbd7b539f7b5ec56
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-20 01:31:27 +0700

    Menambah fungsi post siswa

--- app/src/main/java/com/example/a029_questapi/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/a029_questapi/apiservice/ServiceApiSiswa.kt
@@ -1,11 +1,14 @@
 package com.example.a029_questapi.apiservice
 
 import com.example.a029_questapi.modeldata.DataSiswa
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ServiceApiSiswa {
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
-    
 
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
