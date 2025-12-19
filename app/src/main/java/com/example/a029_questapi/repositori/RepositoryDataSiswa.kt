567228352438d9313345d14e4efd419cf3723835
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-20 01:35:29 +0700

    Membuat kelas `JaringanRepositoryDataSiswa`

--- app/src/main/java/com/example/a029_questapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/a029_questapi/repositori/RepositoryDataSiswa.kt
@@ -1,8 +1,15 @@
 package com.example.a029_questapi.repositori
 
+import com.example.a029_questapi.apiservice.ServiceApiSiswa
 import com.example.a029_questapi.modeldata.DataSiswa
 
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+) : RepositoryDataSiswa{
+    
 }
