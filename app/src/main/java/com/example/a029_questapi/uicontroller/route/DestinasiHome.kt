d8f1eb8d006fd77c4cd141b2af151a186d5a0d3a
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-20 03:47:20 +0700

    Membuat destinasi home

---
+++ app/src/main/java/com/example/a029_questapi/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.a029_questapi.uicontroller.route
+
+import com.example.a029_questapi.R
+
+object DestinasiHome {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
