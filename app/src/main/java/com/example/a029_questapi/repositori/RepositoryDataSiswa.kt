4b39acef70b9786c3618f18e58af3f5f3af8cf96
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-20 01:38:01 +0700

    Menambahkan fungsi postDataSiswa

--- app/src/main/java/com/example/a029_questapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/a029_questapi/repositori/RepositoryDataSiswa.kt
@@ -12,5 +12,5 @@ class JaringanRepositoryDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
 ) : RepositoryDataSiswa{
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
-
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
 }
