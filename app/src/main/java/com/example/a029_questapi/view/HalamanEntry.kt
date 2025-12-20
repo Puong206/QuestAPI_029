5299159827209ae7d7e1a9182f72543727c5bbc8
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-20 11:57:33 +0700

    Membuat halaman entri siswa

--- app/src/main/java/com/example/a029_questapi/view/HalamanEntry.kt
+++ app/src/main/java/com/example/a029_questapi/view/HalamanEntry.kt
@@ -1,2 +1,33 @@
 package com.example.a029_questapi.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.a029_questapi.viewmodel.EntryViewModel
+import com.example.a029_questapi.viewmodel.provider.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = rememberTopAppBarState()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {}
+    ) { innerPadding ->
+        EntrySiswaBody(
+
+        )
+    }
+}
+
