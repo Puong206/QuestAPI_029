46465e9adf07539f8d4a7117bf4a124e7abbb7d1
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-20 12:09:56 +0700

    Membuat objek destinasi mengimplementasikan DestinasiNavigasi

--- app/src/main/java/com/example/a029_questapi/uicontroller/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/a029_questapi/uicontroller/route/DestinasiEntry.kt
@@ -2,7 +2,7 @@ package com.example.a029_questapi.uicontroller.route
 
 import com.example.a029_questapi.R
 
-object DestinasiEntry {
+object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
 }
--- app/src/main/java/com/example/a029_questapi/uicontroller/route/DestinasiHome.kt
+++ app/src/main/java/com/example/a029_questapi/uicontroller/route/DestinasiHome.kt
@@ -2,7 +2,7 @@ package com.example.a029_questapi.uicontroller.route
 
 import com.example.a029_questapi.R
 
-object DestinasiHome {
+object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
 }
