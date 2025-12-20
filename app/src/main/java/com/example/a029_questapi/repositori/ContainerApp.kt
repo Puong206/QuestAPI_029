54562f8e314739736f8ae949ea04392a1d048161
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-20 12:31:16 +0700

    Menambahkan base URL

--- app/src/main/java/com/example/a029_questapi/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/a029_questapi/repositori/ContainerApp.kt
@@ -14,7 +14,7 @@ interface ContainerApp {
 }
 
 class DefaultContainerApp : ContainerApp {
-    private val baseurl = ""
+    private val baseurl = "http://10.0.2.2/umyTI/"
 
     val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
