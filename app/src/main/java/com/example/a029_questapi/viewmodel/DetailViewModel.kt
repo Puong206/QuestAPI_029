259d3b1c708fd73502e2489da9ed37e8d6c1c802
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-26 15:09:20 +0700

    Menambah status UI dan mengambil id siswa di DetailViewModel

--- app/src/main/java/com/example/a029_questapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/a029_questapi/viewmodel/DetailViewModel.kt
@@ -1,9 +1,13 @@
 package com.example.a029_questapi.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.a029_questapi.modeldata.DataSiswa
 import com.example.a029_questapi.repositori.RepositoryDataSiswa
+import com.example.a029_questapi.uicontroller.route.DestinasiDetail
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
@@ -13,5 +17,8 @@ sealed interface StatusUIDetail {
 
 class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
 RepositoryDataSiswa): ViewModel() {
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
 
 }
