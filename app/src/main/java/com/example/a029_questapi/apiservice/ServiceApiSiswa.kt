216b7fa1e7452ef5f37266b6ad5aae4ab65347ac
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-26 13:53:11 +0700

    Menambah fungsi edit dan delete siswa

--- app/src/main/java/com/example/a029_questapi/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/a029_questapi/apiservice/ServiceApiSiswa.kt
@@ -2,8 +2,10 @@ package com.example.a029_questapi.apiservice
 
 import com.example.a029_questapi.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Query
 
 interface ServiceApiSiswa {
@@ -16,5 +18,9 @@ interface ServiceApiSiswa {
     @GET("baca1Teman.php/{id}")
     suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
 
+    @PUT("editTM.php/{id")
+    suspend fun editSatuSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 
+    @DELETE("deleteTM.php/{id}")
+    suspend fun deleteSatuSiswa(@Query("id") id: Int):retrofi
 }
