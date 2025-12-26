0079646cff47992ccaa4a231e34c644c1cc85b6c
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-26 15:01:35 +0700

    Menambah fungsi suspend untuk edit siswa

--- app/src/main/java/com/example/a029_questapi/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/a029_questapi/viewmodel/EditViewModel.kt
@@ -8,10 +8,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.a029_questapi.modeldata.DetailSiswa
 import com.example.a029_questapi.modeldata.UIStateSiswa
+import com.example.a029_questapi.modeldata.toDataSiswa
 import com.example.a029_questapi.modeldata.toUiStateSiswa
 import com.example.a029_questapi.repositori.RepositoryDataSiswa
 import com.example.a029_questapi.uicontroller.route.DestinasiDetail
 import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class EditViewModel(savedStateHandle: SavedStateHandle,private val repositoryDataSiswa:
 RepositoryDataSiswa): ViewModel() {
@@ -34,6 +36,19 @@ RepositoryDataSiswa): ViewModel() {
     private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
         return with(uiState) {
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    suspend fun editSatuSiswa() {
+        if (validasiInput(uiStateSiswa.detailSiswa)) {
+            val call: Response<Void> = repositoryDataSiswa.editSatuSiswa(idSiswa, uiStateSiswa
+                .detailSiswa.toDataSiswa())
+
+            if (call.isSuccessful) {
+                println("Update Sukses : ${call.message()}")
+            } else {
+                println("Update Gagal : ${call.errorBody()}")
+            }
         }
     }
 }
