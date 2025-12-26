9a90f08790420a75198867f1bd1106781f152472
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-26 14:04:58 +0700

    Membuat destinasi untuk detail siswa

---
+++ app/src/main/java/com/example/a029_questapi/uicontroller/route/DestinasiDetail.kt
@@ -0,0 +1,10 @@
+package com.example.a029_questapi.uicontroller.route
+
+import com.example.a029_questapi.R
+
+object DestinasiDetail: DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
