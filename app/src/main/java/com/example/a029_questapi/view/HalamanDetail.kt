e8ad65f0a1a86299379310ddc298a3334b351354
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-26 15:52:01 +0700

    Menambahkan composable `BarisDetailData`

--- app/src/main/java/com/example/a029_questapi/view/HalamanDetail.kt
+++ app/src/main/java/com/example/a029_questapi/view/HalamanDetail.kt
@@ -1,8 +1,14 @@
 package com.example.a029_questapi.view
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.a029_questapi.viewmodel.DetailViewModel
 import com.example.a029_questapi.viewmodel.provider.PenyediaViewModel
@@ -16,4 +22,15 @@ fun DetailSiswaScreen(
     viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
 
+}
+
+@Composable
+private fun BarisDetailData(
+    @StringRes labelResID: Int, itemDetail: String, modifier: Modifier = Modifier
+){
+    Row(modifier = Modifier) {
+        Text(stringResource(labelResID))
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
+    }
 }
