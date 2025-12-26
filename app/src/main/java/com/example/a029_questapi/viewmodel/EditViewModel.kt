2a587ec0c65798faf776f1cd98f8e74ed2980b44
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-26 14:47:13 +0700

    Membuat EditViewModel untuk mengambil data siswa tunggal

--- app/src/main/java/com/example/a029_questapi/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/a029_questapi/viewmodel/EditViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.a029_questapi.viewmodel
 
-class EditViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a029_questapi.modeldata.UIStateSiswa
+import com.example.a029_questapi.modeldata.toUiStateSiswa
+import com.example.a029_questapi.repositori.RepositoryDataSiswa
+import com.example.a029_questapi.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(savedStateHandle: SavedStateHandle,private val repositoryDataSiswa:
+RepositoryDataSiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
 }
