ba79337082cf09b624cdac98733d13ab7f87a367
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-26 15:04:46 +0700

    Membuat Status UI dan DetailViewModel

--- app/src/main/java/com/example/a029_questapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/a029_questapi/viewmodel/DetailViewModel.kt
@@ -1,9 +1,17 @@
 package com.example.a029_questapi.viewmodel
 
-sealed interface StatusUIDetail {
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.a029_questapi.modeldata.DataSiswa
+import com.example.a029_questapi.repositori.RepositoryDataSiswa
 
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
 }
 
-class DetailViewModel() {
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
+RepositoryDataSiswa): ViewModel() {
 
 }
