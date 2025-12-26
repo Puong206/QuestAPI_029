5a277dab7a4f3c93e1cc7145e4b6ddede23e3736
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-26 15:46:41 +0700

    Menambah parameter untuk `DetailSiswaScreen`

--- app/src/main/java/com/example/a029_questapi/view/HalamanDetail.kt
+++ app/src/main/java/com/example/a029_questapi/view/HalamanDetail.kt
@@ -2,9 +2,18 @@ package com.example.a029_questapi.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.a029_questapi.viewmodel.DetailViewModel
+import com.example.a029_questapi.viewmodel.provider.PenyediaViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateToBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
 
-){}
+}
