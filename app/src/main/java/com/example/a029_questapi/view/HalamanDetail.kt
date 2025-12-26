09e7ba7d88f0f3866eef112f8cc27a6ad53531c2
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-26 15:56:27 +0700

    Menambahkan dialog konfirmasi hapus

--- app/src/main/java/com/example/a029_questapi/view/HalamanDetail.kt
+++ app/src/main/java/com/example/a029_questapi/view/HalamanDetail.kt
@@ -3,13 +3,16 @@ package com.example.a029_questapi.view
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.a029_questapi.R
 import com.example.a029_questapi.viewmodel.DetailViewModel
 import com.example.a029_questapi.viewmodel.provider.PenyediaViewModel
 
@@ -33,4 +36,28 @@ private fun BarisDetailData(
         Spacer(modifier = Modifier.weight(1f))
         Text(text = itemDetail, fontWeight = FontWeight.Bold)
     }
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    AlertDialog(
+        onDismissRequest = {},
+        title = { Text(stringResource(R.string.attention))},
+        text = { Text(stringResource(R.string.delete))},
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(stringResource(R.string.no))
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(stringResource(R.string.yes))
+            }
+        }
+    )
 }
