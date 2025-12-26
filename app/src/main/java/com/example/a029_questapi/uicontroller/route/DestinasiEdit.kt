c3216f6b7ff27bcabf346a634c4b74f5dda206dc
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-12-26 14:08:53 +0700

    Memperbarui DestinasiEdit dengan route, title, dan argumen navigasi

--- app/src/main/java/com/example/a029_questapi/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/a029_questapi/uicontroller/route/DestinasiEdit.kt
@@ -1,4 +1,10 @@
 package com.example.a029_questapi.uicontroller.route
 
-object DestinasiEdit {
+import com.example.a029_questapi.R
+
+object DestinasiEdit: DestinasiNavigasi {
+    override val route = "edit_siswa"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
